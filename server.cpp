f9c8b00830cc6bb9607090c5b6a17f08d5878573
Shayan <amirhossein.shahgholi@divar.ir>
2023-03-12 14:13:14 +0330

    Add signup and signin functions

--- server.cpp
+++ server.cpp
@@ -13,6 +13,22 @@
 #include <cstring>
 #include <regex>
 
+class User {
+public:
+    int id;
+    std::string username;
+    std::string password;
+    std::string isAdmin;
+    std::string purse;
+    std::string phoneNumber;
+    std::string address;
+};
+std::vector<User> users;
+User null_user;
+
+void print_message(int msg_code){
+    printf("%d\n", msg_code);
+}
 
 int setupServer(int port, std::string addr) {
     struct sockaddr_in address;
@@ -72,23 +88,114 @@ char* validate_date_time(char buff[1024]){
     return {0};
 }
 
+void print_users_info(){
+    for (const auto& user : users) {
+        std::cout << "ID: " << user.id << std::endl;
+        std::cout << "Username: " << user.username << std::endl;
+        std::cout << "Password: " << user.password << std::endl;
+        std::cout << "Is admin: " << user.isAdmin << std::endl;
+        std::cout << "Purse: " << user.purse << std::endl;
+        std::cout << "Phone number: " << user.phoneNumber << std::endl;
+        std::cout << "Address: " << user.address << std::endl;
+        std::cout << std::endl;
+    }
+}
+
+void sign_in(std::string username, std::string password){
+    for (const auto& user : users) {
+        if (user.username == username && user.password == password){
+            print_message(203);
+            return;
+        }
+    }
+    raise_error(430);
+    return;
+}
+
+void create_users(){
+    std::ifstream jsonFile("UsersInfo.json");
+    nlohmann::json j;
+    jsonFile >> j;
+    for (auto& _user : j["users"]) {
+        User user;
+        user.id = _user["id"];
+        user.username = _user["user"];
+        user.password = _user["password"];
+        user.isAdmin = _user["admin"];
+
+        if (_user.find("purse") != _user.end()) {
+            user.purse = _user["purse"];
+        }
+        if (_user.find("phoneNumber") != _user.end()) {
+            user.phoneNumber = _user["phoneNumber"];
+        }
+        if (_user.find("address") != _user.end()) {
+            user.address = _user["address"];
+        }
+
+        users.push_back(user);
+    }
+    return;
+}
+
+User complete_user_signup(User user, std::string data, int data_type){
+    if (data.length() == 1){
+        data_type = -1;
+    }
+    switch (data_type){
+        case 0: //PASSWORD
+            user.password = data;
+            break;
+        case 1: //PURSE
+            user.purse = data;
+            break;
+        case 2: //PHONE NUMBER
+            user.phoneNumber = data;
+            break;
+        case 3: // ADDRESS
+            user.address = data;
+            break;
+        default: //Invalid
+            raise_error(503);
+
+    }
+    return user;
+
+}
+
+User user_signup(std::string username){
+    for (const auto& user : users) {
+        if (user.username == username ){
+            raise_error(451);
+            return null_user;
+        }
+    }
+    User new_user;
+    print_message(311);
+    new_user.username = username;
+    return new_user;
+}
+
+void save_user (User user){
+    user.isAdmin = "false";
+    users.push_back(user);
+    print_message(231);
+}
+
 int main(int argc, char const *argv[]) {
     int server_fd, new_socket, max_sd;
     char buffer[1024] = {0};
     fd_set master_set, working_set;
     std::string addr;
     int port;
+    std::string date_time;
     read_config_file(&addr, &port);
+    create_users();
     server_fd = setupServer(port, addr);
     FD_ZERO(&master_set);
     max_sd = server_fd;
     FD_SET(server_fd, &master_set);
     write(1, "Server is running\n", 18);
-    write(1, "Enter Date time(Ex. 25-02-2023)>>", 33);
-    char buff[1024] = {0};
-    read(0, buff, 1024);
-    std::string date_time = validate_date_time(buff);
-    std::cout<<date_time<<std::endl;
     while (1) {
         working_set = master_set;
         select(max_sd + 1, &working_set, NULL, NULL, NULL);
